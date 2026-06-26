bf3d6c218578868eca84d0e06ef202b89ade43d7
Moisés <moises@e-mail.com.br>
2026-06-26 13:39:15 -0300

    Creating menu interactions Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/level.py
+++ code/level.py
@@ -9,20 +9,22 @@ from code.const import COLOR_WHITE, WIN_HEIGHT
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
+
 class Level:
     def __init__(self, window, name, game_mode):
+        self.timeout = 0
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-
+        self.timeout = 20000 # 20 segundos
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.set_volume(0.3)
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
-
         while True:
             clock.tick(60)
             for ent in self.entity_list:
@@ -34,19 +36,16 @@ class Level:
                     sys.exit()
 
             # printed text
-            self.level_text(text_size=14, text=f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE,text_pos=(10, 5))
-            self.level_text(text_size=14, text=f'fps: {clock.get_fps() :.0f}', COLOR_WHITE,text_pos=(10, WIN_HEIGHT - 35))
-            self.level_text(text_size=14, text=f'entidades: {len(self.entity_list)}', COLOR_WHITE,text_pos=(10, WIN_HEIGHT - 20))
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
-
         pass
 
-        def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
-            text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-            text_surf: Surface = text_font.render(text, antialias=True, text_color).convert_alpha()
-            text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
-            self.window.blit(source=text_surf, dest=text_rect)
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
 
-        pass
-
-
+    pass
